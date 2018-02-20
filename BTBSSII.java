3d868e17502deb93f8289b04e374755e0629d30f
Eclat001 <cyeclat@gmail.com>
2018-02-19 18:18:08 -0600

    021918 leetcode facebook
    Array; Greedy

---
+++ BTBSSII.java
@@ -0,0 +1,13 @@
+package facebook;
+
+public class BTBSSII {
+	//Time O(n) Space O(1)
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+}
