6dab6ff1c51cc672af77ed504e041b870f924161
Eclat001 <cyeclat@gmail.com>
2018-02-20 22:01:58 -0600

    022018 leetcode facebook
    Array; Hash Table

---
+++ TwoSum 2.java
@@ -0,0 +1,29 @@
+package facebook;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] rst = new int[2];
+        if (nums == null || nums.length < 2) {
+            return rst;
+        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int ans = target - nums[i];
+            if (map.containsKey(ans) && map.get(ans) != i) {
+                rst[0] = map.get(ans);
+                rst[1] = i;
+            } 
+            map.put(nums[i], i);
+        }
+        return rst;
+    }
+	
+    public static void main(String[] args) {
+    	TwoSum ts = new TwoSum();
+    	int[] nums = {2, 7, 11, 15};
+    	int target = 9;
+    	System.out.println(ts.twoSum(nums, target));
+    }
+}
