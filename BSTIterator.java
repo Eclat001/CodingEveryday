e074f06bb17f89f1dbbe40d1d07ecdbf955d5a0f
Eclat001 <cyeclat@gmail.com>
2018-02-20 21:03:56 -0600

    022018 leetcode facebook
    Stack; Tree; Design

---
+++ BSTIterator.java
@@ -0,0 +1,42 @@
+package facebook;
+
+import java.util.Stack;
+
+import structure.TreeNode;
+
+public class BSTIterator {
+	Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        int rst = node.val;
+        if (node.right != null) {
+            node = node.right;
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+        return rst;
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
